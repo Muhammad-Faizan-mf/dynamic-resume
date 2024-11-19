04a5f58d5985e0af8108d077ea4fe1f26fef98cd
Muhammad-Faizan-mf <60542079+Muhammad-Faizan-mf@users.noreply.github.com>
2024-11-19 15:59:14 +0500

    updated js

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ form.addEventListener('submit', function (event) {
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var phone = document.getElementById('phone').value;
+    var cnic = document.getElementById('cnic').value;
     var education = document.getElementById('education').value;
     var experience = document.getElementById('experience').value;
     var skills = document.getElementById('skills').value;
-    var resumeHtML = "\n    <h2>Resume</h2>\n    <h3>Personal Details</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Email:</b>").concat(email, "</p>\n    <p><b>Phone:</b>").concat(phone, "</p>\n\n    <h3>Education</h3>\n    <p><").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p><").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p><").concat(skills, "</p>\n    ");
+    var resumeHtML = "\n    <h2>Resume</h2>\n    <h3>Personal Details</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Email:</b>").concat(email, "</p>\n    <p><b>Phone:</b>").concat(phone, "</p>\n    <p><b>CNIC:</b>").concat(cnic, "</p>\n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skills, "</p>\n    ");
     if (resumeDisplayElement) {
         resumeDisplayElement.innerHTML = resumeHtML;
     }
